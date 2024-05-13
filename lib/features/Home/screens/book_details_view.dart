f07e97f7bfcb82a43f58f5b0ab5f262e1c2d4143
MahmoudAlMarashdy <mahmoudalmarashdy.dev.m@gmail.com>
2024-05-13 04:34:37 +0300

    book details view init

---
+++ lib/features/Home/screens/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:books_up/features/Home/screens/widgets/book_details_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookDetailsBody(),
+    );
+  }
+}
