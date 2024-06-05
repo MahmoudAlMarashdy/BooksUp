44e3453c3f68d2928f118fd71960f98cf00d2233
MahmoudAlMarashdy <mahmoudalmarashdy.dev.m@gmail.com>
2024-06-05 09:22:53 +0300

    featured books cubit done

--- lib/features/Home/presentation/manager/featured_books/cubit/featured_books_cubit.dart
+++ lib/features/Home/presentation/manager/featured_books/cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:books_up/features/Home/domain/entities/book_entity.dart';
+import 'package:books_up/features/Home/domain/use_cases/featured_books_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+
+  final FeaturedBooksUseCase featuredBooksUseCase;
+
+  Future<void> fetchFeaturedBooks()async {
+    emit(FeaturedBooksLoading());
+
+    var result = await featuredBooksUseCase.Execute();
+
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.failureMessage)),
+      (featuredBooks) => emit(FeaturedBooksSuccess(featuredBooks))
+    );
+  }
 }
