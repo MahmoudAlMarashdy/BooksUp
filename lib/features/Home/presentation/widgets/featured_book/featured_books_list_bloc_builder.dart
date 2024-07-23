30835aef94514cc98d36bd4fc7cd85dac48c5cbd
MahmoudAlMarashdy <mahmoudalmarashdy.dev.m@gmail.com>
2024-07-23 09:13:09 +0300

    displaying featured and newest books

---
+++ lib/features/Home/presentation/widgets/featured_book/featured_books_list_bloc_builder.dart
@@ -0,0 +1,26 @@
+import 'package:books_up/features/Home/presentation/controller/featured_books/cubit/featured_books_cubit.dart';
+import 'package:books_up/features/Home/presentation/widgets/featured_book/featured_books_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeatureBooksListBlocBuilder extends StatelessWidget {
+  const FeatureBooksListBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if(state is FeaturedBooksSuccess){
+          return FeaturedBooksList();
+        }else if(state is FeaturedBooksFailure){
+          print(state.errorMessage);
+          return Text(state.errorMessage);
+        }else{
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
